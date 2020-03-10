b196d7c150e0e5a7acac4c6a2ad5315967f49860
simon-kingston <simonkingston@eventmapsolutions.com>
2020-03-10 22:10:04 +0000

    first Drivers championship implementation

---
+++ R/finalise_plot.R
@@ -0,0 +1,122 @@
+library(readr)
+library(ggplot2)
+
+myfile <- "https://raw.githubusercontent.com/simon-kingston/2019-FIA-f1-World-Championship/master/data/2019-f1-results.csv"
+
+f1_data <- read_csv(myfile)
+
+drivers <- unique(f1_data$Driver)
+teams <- sort(unique(f1_data$Team))
+
+omit <- function(race){
+  c(na.omit(race))
+}
+
+raw_driver_results <- matrix(c(omit(f1_data$AUS), omit(f1_data$BHR), omit(f1_data$CHN), omit(f1_data$AZE), omit(f1_data$ESP), omit(f1_data$MON), omit(f1_data$CAN), omit(f1_data$FRA), omit(f1_data$AUT),  omit(f1_data$GBR), 
+                               omit(f1_data$GER), omit(f1_data$HUN), omit(f1_data$BEL), omit(f1_data$ITA), omit(f1_data$SIN), omit(f1_data$RUS), omit(f1_data$JPN), omit(f1_data$MEX), omit(f1_data$USA), omit(f1_data$BRA), omit(f1_data$ABU)), nrow = 21, ncol = 20, byrow = TRUE)
+
+
+
+is_Fastest_Lap <- function(position){
+  grepl("F", position)
+}
+
+parse_int <- function(position){
+  to_int(gsub("[a-zA-Z ]", "", position))
+}
+
+fastest_lap <- function(driver_position, driver_points, score_system, fastest_lap_point){
+  f_points <- driver_points
+  if (driver_position <  11 && !is.na(driver_position)){
+    f_points <- f_points + score_system[driver_position] + fastest_lap_point
+  }
+  f_points
+}
+
+
+to_int <- function(a){
+  as.integer(a)
+}
+
+dnf <- function(a){
+  ifelse(a%in%c("DSQ", "RET"), TRUE, FALSE)
+}
+
+update_points <- function(data_set, rows){
+  points <- numeric(20)
+  score_system <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+  fastest_lap_point <- 1
+  
+  for(row in 1:rows) {
+    
+    for (id in 1:20) {
+      if (dnf(data_set[row, id])){
+        print("a")
+      } else if (is_Fastest_Lap(data_set[row, id])) {
+        # FASTEST LAP
+        points[id] <- fastest_lap(parse_int(data_set[row, id]), points[id],
+                                  score_system, fastest_lap_point)
+        
+      } else if (!is.na(to_int(data_set[row, id]))) {
+        # Regular Points
+        points[id] <- points[id] + score_system[to_int(data_set[row, id])]
+        
+      } 
+      
+    }
+  }
+  
+  points
+}
+
+r1 = update_points(raw_driver_results, 1)
+r2 = update_points(raw_driver_results, 2)
+r3 = update_points(raw_driver_results, 3)
+r4 = update_points(raw_driver_results, 4)
+r5 = update_points(raw_driver_results, 5)
+r6 = update_points(raw_driver_results, 6)
+r7 = update_points(raw_driver_results, 7)
+r8 = update_points(raw_driver_results, 8)
+r9 = update_points(raw_driver_results, 9)
+r10 = update_points(raw_driver_results, 10)
+r11 = update_points(raw_driver_results, 11)
+r12 = update_points(raw_driver_results, 12)
+r13 = update_points(raw_driver_results, 13)
+r14 = update_points(raw_driver_results, 14)
+r15 = update_points(raw_driver_results, 15)
+r16 = update_points(raw_driver_results, 16)
+r17 = update_points(raw_driver_results, 17)
+r18 = update_points(raw_driver_results, 18)
+r19 = update_points(raw_driver_results, 19)
+r20 = update_points(raw_driver_results, 20)
+r21 = update_points(raw_driver_results, 21)
+
+races <- factor(names(f1_data)[3:23], levels = names(f1_data)[3:23])
+df1 <-  data.frame(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21)
+
+var0 <- as.numeric(df1[1, ])
+var1 <- as.numeric(df1[2, ])
+
+ggplot() + 
+  geom_line(aes(x=races,y=as.numeric(df1[1, ])), color='cyan', group = 1) + # HAM
+  geom_line(aes(x=races,y=as.numeric(df1[2, ])), color='cyan', group = 1) + # BOT
+  geom_line(aes(x=races,y=as.numeric(df1[3, ])), color='blue', group = 1) + # VER
+  geom_line(aes(x=races,y=as.numeric(df1[4, ])), color='red', group = 1) + # LEC
+  geom_line(aes(x=races,y=as.numeric(df1[5, ])), color='red', group = 1) + # VET
+  geom_line(aes(x=races,y=as.numeric(df1[6, ])), color='orange', group = 1) + # SAI
+  geom_line(aes(x=races,y=as.numeric(df1[7, ])), color='light blue', group = 1) + # GAS
+  geom_line(aes(x=races,y=as.numeric(df1[8, ])), color='blue', group = 1) + # ALB
+  geom_line(aes(x=races,y=as.numeric(df1[9, ])), color='yellow', group = 1) + # RIC
+  geom_line(aes(x=races,y=as.numeric(df1[10, ])), color='pink', group = 1) + # PER
+  geom_line(aes(x=races,y=as.numeric(df1[11, ])), color='orange', group = 1) + # NOR
+  geom_line(aes(x=races,y=as.numeric(df1[12, ])), color='dark red', group = 1) + # RAI
+  geom_line(aes(x=races,y=as.numeric(df1[13, ])), color='light blue', group = 1) + # KYV
+  geom_line(aes(x=races,y=as.numeric(df1[14, ])), color='yellow', group = 1) + # HUL
+  geom_line(aes(x=races,y=as.numeric(df1[15, ])), color='gold', group = 1) + # MAG
+  geom_line(aes(x=races,y=as.numeric(df1[16, ])), color='pink', group = 1) + # STR
+  geom_line(aes(x=races,y=as.numeric(df1[17, ])), color='dark red', group = 1) + # GIO
+  geom_line(aes(x=races,y=as.numeric(df1[18, ])), color='gold', group = 1) + # # GRO
+  geom_line(aes(x=races,y=as.numeric(df1[19, ])), color='grey', group = 1) + # KUB
+  geom_line(aes(x=races,y=as.numeric(df1[20, ])), color='grey', group = 1) + # RUS
+  ylab('Drivers Championship')+xlab('Race')+ggtitle("2019 F1 Driver Championship") +
+  theme(plot.title = element_text(hjust = 0.5))
