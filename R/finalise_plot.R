4e965e5e48a61b18945b1afdfb6f735a98ddc562
simon-kingston <simonkingston@eventmapsolutions.com>
2020-03-11 10:35:35 +0000

    Add Score System bar chart
    updated titles
    updated readme

--- R/finalise_plot.R
+++ R/finalise_plot.R
@@ -1,12 +1,13 @@
 library(readr)
 library(ggplot2)
 
-myfile <- "https://raw.githubusercontent.com/simon-kingston/2019-FIA-f1-World-Championship/master/data/2019-f1-results.csv"
+myfile <- 'https://raw.githubusercontent.com/simon-kingston/2019-FIA-f1-World-Championship/master/data/2019-f1-results.csv'
 
 f1_data <- read_csv(myfile)
 
 drivers <- unique(f1_data$Driver)
 teams <- sort(unique(f1_data$Team))
+score_system <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 omit <- function(race){
   c(na.omit(race))
@@ -18,11 +19,11 @@ raw_driver_results <- matrix(c(omit(f1_data$AUS), omit(f1_data$BHR), omit(f1_dat
 
 
 is_Fastest_Lap <- function(position){
-  grepl("F", position)
+  grepl('F', position)
 }
 
 parse_int <- function(position){
-  to_int(gsub("[a-zA-Z ]", "", position))
+  to_int(gsub('[a-zA-Z ]', '', position))
 }
 
 fastest_lap <- function(driver_position, driver_points, score_system, fastest_lap_point){
@@ -39,19 +40,19 @@ to_int <- function(a){
 }
 
 dnf <- function(a){
-  ifelse(a%in%c("DSQ", "RET"), TRUE, FALSE)
+  ifelse(a%in%c('DSQ', 'RET'), TRUE, FALSE)
 }
 
-update_points <- function(data_set, rows){
+update_points <- function(data_set, rows, score_system){
   points <- numeric(20)
-  score_system <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+  
   fastest_lap_point <- 1
   
   for(row in 1:rows) {
     
     for (id in 1:20) {
       if (dnf(data_set[row, id])){
-        print("a")
+
       } else if (is_Fastest_Lap(data_set[row, id])) {
         # FASTEST LAP
         points[id] <- fastest_lap(parse_int(data_set[row, id]), points[id],
@@ -69,30 +70,25 @@ update_points <- function(data_set, rows){
   points
 }
 
-r1 = update_points(raw_driver_results, 1)
-r2 = update_points(raw_driver_results, 2)
-r3 = update_points(raw_driver_results, 3)
-r4 = update_points(raw_driver_results, 4)
-r5 = update_points(raw_driver_results, 5)
-r6 = update_points(raw_driver_results, 6)
-r7 = update_points(raw_driver_results, 7)
-r8 = update_points(raw_driver_results, 8)
-r9 = update_points(raw_driver_results, 9)
-r10 = update_points(raw_driver_results, 10)
-r11 = update_points(raw_driver_results, 11)
-r12 = update_points(raw_driver_results, 12)
-r13 = update_points(raw_driver_results, 13)
-r14 = update_points(raw_driver_results, 14)
-r15 = update_points(raw_driver_results, 15)
-r16 = update_points(raw_driver_results, 16)
-r17 = update_points(raw_driver_results, 17)
-r18 = update_points(raw_driver_results, 18)
-r19 = update_points(raw_driver_results, 19)
-r20 = update_points(raw_driver_results, 20)
-r21 = update_points(raw_driver_results, 21)
+r1 = update_points(raw_driver_results, 1, score_system)
+r2 = update_points(raw_driver_results, 2, score_system)
+r3 = update_points(raw_driver_results, 3, score_system)
+r4 = update_points(raw_driver_results, 4, score_system)
+r5 = update_points(raw_driver_results, 5, score_system)
+r6 = update_points(raw_driver_results, 6, score_system)
+r7 = update_points(raw_driver_results, 7, score_system)
+r13 = update_points(raw_driver_results, 13, score_system)
+r14 = update_points(raw_driver_results, 14, score_system)
+r15 = update_points(raw_driver_results, 15, score_system)
+r16 = update_points(raw_driver_results, 16, score_system)
+r17 = update_points(raw_driver_results, 17, score_system)
+r18 = update_points(raw_driver_results, 18, score_system)
+r19 = update_points(raw_driver_results, 19, score_system)
+r20 = update_points(raw_driver_results, 20, score_system)
+r21 = update_points(raw_driver_results, 21, score_system)
 
 races <- factor(names(f1_data)[3:23], levels = names(f1_data)[3:23])
-df1 <-  data.frame(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21)
+df1 <- data.frame(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21)
 
 var0 <- as.numeric(df1[1, ])
 var1 <- as.numeric(df1[2, ])
@@ -118,5 +114,15 @@ ggplot() +
   geom_line(aes(x=races,y=as.numeric(df1[18, ])), color='gold', group = 1) + # # GRO
   geom_line(aes(x=races,y=as.numeric(df1[19, ])), color='grey', group = 1) + # KUB
   geom_line(aes(x=races,y=as.numeric(df1[20, ])), color='grey', group = 1) + # RUS
-  ylab('Drivers Championship')+xlab('Race')+ggtitle("2019 F1 Driver Championship") +
+  ylab('Points')+xlab('Race')+ggtitle('2019 F1 Driver Championship') +
   theme(plot.title = element_text(hjust = 0.5))
+
+ss <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 1)
+ss_header <- c('1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', 'Fastest Lap')
+df_ss <- data.frame(position=factor(ss_header, levels=ss_header), points=ss)
+
+ggplot(data=df_ss, aes(x=position, y=points))+geom_bar(stat='identity',fill="red",color='white') + 
+ylab('Points Awarded')+xlab('Position')+ggtitle('Scoring System')+theme(plot.title = element_text(hjust = 0.5))
+
+ 
+
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 2019-FIA-f1-World-Championship
 2019 FIA Formula One World Championship results modelled in R
 
-**Drivers Championship Standings**
-![](https://github.com/simon-kingston/2019-FIA-f1-World-Championship/blob/master/chart_examples/2019_f1_drivers_championship.png)
+
+![](https://github.com/simon-kingston/2019-FIA-f1-World-Championship/blob/master/chart_examples/2019_f1_drivers_championship.png) ![](https://github.com/simon-kingston/2019-FIA-f1-World-Championship/blob/master/chart_examples/2019_f1_scoring_system.png)
+
