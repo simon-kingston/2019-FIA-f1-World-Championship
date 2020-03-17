0c2defc000ee0317a3c50fb3ddcef17eb0a936e9
simon-kingston <simonkingston@eventmapsolutions.com>
2020-03-17 23:11:28 +0000

    Add cumsum function

--- R/finalise_plot.R
+++ R/finalise_plot.R
@@ -2,134 +2,106 @@ library(readr)
 library(ggplot2)
 library(plotly)
 
-myfile <- 'https://raw.githubusercontent.com/simon-kingston/2019-FIA-f1-World-Championship/master/data/2019-f1-results.csv'
-
-f1_data <- read_csv(myfile)
-
-drivers <- unique(f1_data$Driver)
-teams <- sort(unique(f1_data$Team))
-score_system <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-
 omit <- function(race){
   c(na.omit(race))
 }
-
-raw_driver_results <- matrix(c(omit(f1_data$AUS), omit(f1_data$BHR), omit(f1_data$CHN), omit(f1_data$AZE), omit(f1_data$ESP), omit(f1_data$MON), omit(f1_data$CAN), omit(f1_data$FRA), omit(f1_data$AUT),  omit(f1_data$GBR), 
-                               omit(f1_data$GER), omit(f1_data$HUN), omit(f1_data$BEL), omit(f1_data$ITA), omit(f1_data$SIN), omit(f1_data$RUS), omit(f1_data$JPN), omit(f1_data$MEX), omit(f1_data$USA), omit(f1_data$BRA), omit(f1_data$ABU)), nrow = 21, ncol = 20, byrow = TRUE)
-
-
 
 is_Fastest_Lap <- function(position){
   grepl('F', position)
 }
 
-parse_int <- function(position){
-  to_int(gsub('[a-zA-Z ]', '', position))
+parse_int <- function(chr){
+  to_int(gsub('[a-zA-Z ]', '', chr))
 }
 
-fastest_lap <- function(driver_position, driver_points, score_system, fastest_lap_point){
-  f_points <- driver_points
-  if (driver_position <  11 && !is.na(driver_position)){
-    f_points <- f_points + score_system[driver_position] + fastest_lap_point
-  }
-  f_points
+fastest_lap <- function(position, points, score_system, fastest_lap_point){
+  ifelse(position<=10 && !is.na(position), points+score_system[position]+fastest_lap_point, points)
 }
 
-
-to_int <- function(a){
-  as.integer(a)
+to_int <- function(value){
+  as.integer(value)
 }
 
-dnf <- function(a){
-  ifelse(a%in%c('DSQ', 'RET'), TRUE, FALSE)
+isDNF <- function(result){
+  ifelse(result %in% c('DSQ', 'RET'), TRUE, FALSE)
 }
 
-update_points <- function(data_set, rows, score_system){
+myfile <- 'https://raw.githubusercontent.com/simon-kingston/2019-FIA-f1-World-Championship/master/data/2019-f1-results.csv'
+f1_data <- read_csv(myfile)
+drivers <- unique(f1_data$Driver)
+teams <- sort(unique(f1_data$Team))
+# 11th score is the fastest driver bonus point
+score_system <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 1) 
+
+
+race_standings <- matrix(c(omit(f1_data$AUS), omit(f1_data$BHR), omit(f1_data$CHN), omit(f1_data$AZE), omit(f1_data$ESP), omit(f1_data$MON), omit(f1_data$CAN), omit(f1_data$FRA), omit(f1_data$AUT),  omit(f1_data$GBR), 
+                               omit(f1_data$GER), omit(f1_data$HUN), omit(f1_data$BEL), omit(f1_data$ITA), omit(f1_data$SIN), omit(f1_data$RUS), omit(f1_data$JPN), omit(f1_data$MEX), omit(f1_data$USA), omit(f1_data$BRA), omit(f1_data$ABU)), nrow = 21, ncol = 20, byrow = TRUE)
+
+
+
+update_points <- function(data_set, row, score_system){
   points <- numeric(20)
-  
-  fastest_lap_point <- 1
-  
-  for(row in 1:rows) {
-    
-    for (id in 1:20) {
-      if (dnf(data_set[row, id])){
-
-      } else if (is_Fastest_Lap(data_set[row, id])) {
+  for (id in 1:20) {
+    if (!isDNF(data_set[row, id])){
+      if (is_Fastest_Lap(data_set[row, id])) {
         # FASTEST LAP
         points[id] <- fastest_lap(parse_int(data_set[row, id]), points[id],
-                                  score_system, fastest_lap_point)
+                                  score_system, score_system[11])
         
-      } else if (!is.na(to_int(data_set[row, id]))) {
+      } else if (!is.na(to_int(data_set[row, id])) && to_int(data_set[row, id]) <= 10) {
         # Regular Points
         points[id] <- points[id] + score_system[to_int(data_set[row, id])]
-        
-      } 
-      
-    }
+      }  
+    }  
   }
   
   points
 }
 
-r1 = update_points(raw_driver_results, 1, score_system)
-r2 = update_points(raw_driver_results, 2, score_system)
-r3 = update_points(raw_driver_results, 3, score_system)
-r4 = update_points(raw_driver_results, 4, score_system)
-r5 = update_points(raw_driver_results, 5, score_system)
-r6 = update_points(raw_driver_results, 6, score_system)
-r7 = update_points(raw_driver_results, 7, score_system)
-r8 = update_points(raw_driver_results, 8, score_system)
-r9 = update_points(raw_driver_results, 9, score_system)
-r10 = update_points(raw_driver_results, 10, score_system)
-r11 = update_points(raw_driver_results, 11, score_system)
-r12 = update_points(raw_driver_results, 12, score_system)
-r13 = update_points(raw_driver_results, 13, score_system)
-r14 = update_points(raw_driver_results, 14, score_system)
-r15 = update_points(raw_driver_results, 15, score_system)
-r16 = update_points(raw_driver_results, 16, score_system)
-r17 = update_points(raw_driver_results, 17, score_system)
-r18 = update_points(raw_driver_results, 18, score_system)
-r19 = update_points(raw_driver_results, 19, score_system)
-r20 = update_points(raw_driver_results, 20, score_system)
-r21 = update_points(raw_driver_results, 21, score_system)
+driver_results <- function(race_standings, score_system){
+  df1 <- data.frame(update_points(race_standings, 1, score_system))
+  for (id in 2:21){
+    df1 <- cbind(df1, update_points(race_standings, id, score_system))
+  }
+  df1
+}
 
+df1 <- driver_results(race_standings, score_system)
 races <- factor(names(f1_data)[3:23], levels = names(f1_data)[3:23])
-df1 <- data.frame(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21)
-
-var0 <- as.numeric(df1[1, ])
-var1 <- as.numeric(df1[2, ])
+team_colours <- c('cyan', 'blue', 'red', 'orange', 'light blue', 'yellow', 'pink', 'dark red', 'gold', 'grey')
 
 p1 <- ggplot() + 
-  geom_line(aes(x=races,y=as.numeric(df1[1, ])), color='cyan', group = 1) + # HAM
-  geom_line(aes(x=races,y=as.numeric(df1[2, ])), color='cyan', group = 1) + # BOT
-  geom_line(aes(x=races,y=as.numeric(df1[3, ])), color='blue', group = 1) + # VER
-  geom_line(aes(x=races,y=as.numeric(df1[4, ])), color='red', group = 1) + # LEC
-  geom_line(aes(x=races,y=as.numeric(df1[5, ])), color='red', group = 1) + # VET
-  geom_line(aes(x=races,y=as.numeric(df1[6, ])), color='orange', group = 1) + # SAI
-  geom_line(aes(x=races,y=as.numeric(df1[7, ])), color='light blue', group = 1) + # GAS
-  geom_line(aes(x=races,y=as.numeric(df1[8, ])), color='blue', group = 1) + # ALB
-  geom_line(aes(x=races,y=as.numeric(df1[9, ])), color='yellow', group = 1) + # RIC
-  geom_line(aes(x=races,y=as.numeric(df1[10, ])), color='pink', group = 1) + # PER
-  geom_line(aes(x=races,y=as.numeric(df1[11, ])), color='orange', group = 1) + # NOR
-  geom_line(aes(x=races,y=as.numeric(df1[12, ])), color='dark red', group = 1) + # RAI
-  geom_line(aes(x=races,y=as.numeric(df1[13, ])), color='light blue', group = 1) + # KYV
-  geom_line(aes(x=races,y=as.numeric(df1[14, ])), color='yellow', group = 1) + # HUL
-  geom_line(aes(x=races,y=as.numeric(df1[15, ])), color='gold', group = 1) + # MAG
-  geom_line(aes(x=races,y=as.numeric(df1[16, ])), color='pink', group = 1) + # STR
-  geom_line(aes(x=races,y=as.numeric(df1[17, ])), color='dark red', group = 1) + # GIO
-  geom_line(aes(x=races,y=as.numeric(df1[18, ])), color='gold', group = 1) + # # GRO
-  geom_line(aes(x=races,y=as.numeric(df1[19, ])), color='grey', group = 1) + # KUB
-  geom_line(aes(x=races,y=as.numeric(df1[20, ])), color='grey', group = 1) + # RUS
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[1, ]))), color=team_colours[1], group = 1)  +# HAM
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[2, ]))), color=team_colours[1], group = 2) + # BOT
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[3, ]))), color=team_colours[2], group = 3) + # VER
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[4, ]))), color=team_colours[3], group = 4) + # LEC
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[5, ]))), color=team_colours[3], group = 5) + # VET
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[6, ]))), color=team_colours[4], group = 6) + # SAI
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[7, ]))), color=team_colours[5], group = 7, linetype=4) + # GAS
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[8, ]))), color=team_colours[2], group = 8, linetype=4) + # ALB
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[9, ]))), color=team_colours[6], group = 9) + # RIC
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[10, ]))), color=team_colours[7], group = 10) + # PER
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[11, ]))), color=team_colours[4], group = 11) + # NOR
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[12, ]))), color=team_colours[8], group = 12) + # RAI
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[13, ]))), color=team_colours[5], group = 13) + # KYV
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[14, ]))), color=team_colours[6], group = 14) + # HUL
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[15, ]))), color=team_colours[9], group = 15) + # MAG
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[16, ]))), color=team_colours[7], group = 16) + # STR
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[17, ]))), color=team_colours[8], group = 17) + # GIO
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[18, ]))), color=team_colours[9], group = 18) + # # GRO
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[19, ]))), color=team_colours[10], group = 19) + # KUB
+  geom_line(aes(x=races,y=cumsum(as.numeric(df1[20, ]))), color=team_colours[10], group = 20) + # RUS
   ylab('Points')+xlab('Race')+ggtitle('2019 F1 Drivers Championship') +
   theme(plot.title = element_text(hjust = 0.5))+ theme_bw()
+
 
 fig1 <- ggplotly(p1)
 
 fig1
 
-ss <- c(25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 1)
+
 ss_header <- c('1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', 'Fastest Lap')
-df_ss <- data.frame(position=factor(ss_header, levels=ss_header), points=ss)
+df_ss <- data.frame(position=factor(ss_header, levels=ss_header), points=score_system)
 
 p2 <- ggplot(data=df_ss, aes(x=position, y=points))+geom_bar(stat='identity',fill="red",color='white') + 
 ylab('Points Awarded')+xlab('Position')+ggtitle('Scoring System')+theme(plot.title = element_text(hjust = 0.5))+ theme_bw()
@@ -139,8 +111,8 @@ fig2 <- ggplotly(p2)
 fig2 
 
 
-aus_construstors <- c(f1_data$Team[complete.cases(f1_data$AUS)], f1_data$AUS[complete.cases(f1_data$AUS)])
-bel_construstors <- c(f1_data$Team[complete.cases(f1_data$BEL)], f1_data$BEL[complete.cases(f1_data$BEL)])
+# aus_construstors <- c(f1_data$Team[complete.cases(f1_data$AUS)], f1_data$AUS[complete.cases(f1_data$AUS)])
+# bel_construstors <- c(f1_data$Team[complete.cases(f1_data$BEL)], f1_data$BEL[complete.cases(f1_data$BEL)])
 
-constructors_results_table <-  data.frame(teams, AUS = r1, BHR = r2, CHN = r3, AZE = r4, ESP = r5, MON = r6, CAN = r7, FRA = r8,
-                                          AUT = r9, GBR = r10, GER = r11, HUN = r12, BEL = r13, ITA = r14, SIN = r15, RUS = r16, JPN = r17, MEX = r18, USA = r19, BRA = r20, ABU = r21)
+# constructors_results_table <-  data.frame(teams, AUS = r1, BHR = r2, CHN = r3, AZE = r4, ESP = r5, MON = r6, CAN = r7, FRA = r8,
+#                                           AUT = r9, GBR = r10, GER = r11, HUN = r12, BEL = r13, ITA = r14, SIN = r15, RUS = r16, JPN = r17, MEX = r18, USA = r19, BRA = r20, ABU = r21)
